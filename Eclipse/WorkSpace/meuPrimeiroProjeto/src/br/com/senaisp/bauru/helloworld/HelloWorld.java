eb19c440f10819f91cdd7b2225ce54adef2d6870
victorsilva212 <victorcntres@gmail.com>
2025-06-10 21:55:53 -0300

    Aula01 Eclipse

---
+++ Eclipse/WorkSpace/meuPrimeiroProjeto/src/br/com/senaisp/bauru/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package br.com.senaisp.bauru.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá Mundo!!");
+
+	}
+
+}
