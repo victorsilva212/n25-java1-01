00de79813d2320e27f6fd404816596ac380ba26a
victorsilva212 <victorcntres@gmail.com>
2025-06-23 21:59:46 -0300

    Aula07 Eclipse

---
+++ Eclipse/WorkSpace/n25-java1-01/src/br/com/senaisp/bauru/aula07/Exemplo06.java
@@ -0,0 +1,12 @@
+package br.com.senaisp.bauru.aula07;
+
+public class Exemplo06 {
+
+	public static void main(String[] args) {
+		//Listando os argumentos passados para o programa
+		for (String it: args) {
+			System.out.println(it);
+		}
+	}
+
+}
